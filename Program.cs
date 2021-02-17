425e86fdab47bd67c1b28a01e246414e238ba2d4
William <william.marback@hotmail.com>
2021-02-17 10:45:26 +0100

    User-error checking added

--- Program.cs
+++ Program.cs
@@ -14,15 +14,16 @@ namespace Api_ovning
             System.Console.WriteLine("Please enter a valid pokemon's name or ID");
             
             string userPokemon = Console.ReadLine().Trim();
-            while (userPokemon == "")
-            {
-                System.Console.WriteLine("Please enter a valid pokemon's name or ID");
-                userPokemon = Console.ReadLine().Trim();
-            }
-
             RestRequest request = new RestRequest("pokemon/" + userPokemon);
-
             IRestResponse response = client.Get(request);
+            
+            while (userPokemon == "" || (int)response.StatusCode != 200)
+            {
+                System.Console.WriteLine("\nPlease enter a valid pokemon's name or ID");
+                userPokemon = Console.ReadLine().Trim();
+                request = new RestRequest("pokemon/" + userPokemon);
+                response = client.Get(request);
+            }
 
             Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content);
 
