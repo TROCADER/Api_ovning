06448793514d458161bbf555c226bc2d5df3b72a
William <william.marback@hotmail.com>
2021-02-17 11:02:20 +0100

    Specified data

--- Pokemon.cs
+++ Pokemon.cs
@@ -7,22 +7,35 @@ namespace Api_ovning
     public class Pokemon
     {
         public string name;
-        public string height;
-        public string weight;
+        public float height;
+        public int weight;
 
         public List<string> pokemonInfo = new List<string>();
 
         public void PokemonMethod()
         {
+            name = UppercaseFirst(name);
+            height = height/10;
             pokemonInfo.Add("Name: " + name);
-            pokemonInfo.Add("Height: " + weight);
-            pokemonInfo.Add("Weight: " + weight);
-            
+            pokemonInfo.Add("Height: " + height + "m");
+            pokemonInfo.Add("Weight: " + weight + "kg");
+
             System.Console.WriteLine();
             for (int i = 0; i < pokemonInfo.Count; i++)
             {
                 System.Console.WriteLine(pokemonInfo[i]);
             }
+        }
+
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,18 +9,18 @@ namespace Api_ovning
         static void Main(string[] args)
         {
             RestClient client = new RestClient("https://pokeapi.co/api/v2/");
-            
+
             System.Console.WriteLine("Which pokeon do you want to see information about?");
             System.Console.WriteLine("Please enter a valid pokemon's name or ID");
-            
-            string userPokemon = Console.ReadLine().Trim();
+
+            string userPokemon = Console.ReadLine().Trim().ToLower();
             RestRequest request = new RestRequest("pokemon/" + userPokemon);
             IRestResponse response = client.Get(request);
-            
+
             while (userPokemon == "" || (int)response.StatusCode != 200)
             {
                 System.Console.WriteLine("\nPlease enter a valid pokemon's name or ID");
-                userPokemon = Console.ReadLine().Trim();
+                userPokemon = Console.ReadLine().Trim().ToLower();
                 request = new RestRequest("pokemon/" + userPokemon);
                 response = client.Get(request);
             }
