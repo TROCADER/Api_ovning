c342ce161cf0dddd3faea3bd2cfd73d9a8612303
William <william.marback@hotmail.com>
2021-02-17 10:00:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Api_ovning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
