525c5ede4a29b56a28e1cfa943642d61dc18ac64
William <william.marback@hotmail.com>
2021-02-23 11:11:47 +0100

    Looping, encapsulation

--- Pokemon.cs
+++ Pokemon.cs
@@ -6,21 +6,61 @@ namespace Api_ovning
 {
     public class Pokemon : ExtraStuff
     {
-        public string name;
-        public float height;
-        public int weight;
+        // private string name;
+        // private float height;
+        // private int weight;
+
+        // public string Name
+        // {
+        //     set
+        //     {
+        //         name = value;
+        //     }
+        //     get
+        //     {
+        //         return name;
+        //     }
+        // }
+
+        // public float Height
+        // {
+        //     set
+        //     {
+        //         height = value;
+        //     }
+        //     get
+        //     {
+        //         return height;
+        //     }
+        // }
+
+        // public int Weight
+        // {
+        //     set
+        //     {
+        //         weight = value;
+        //     }
+        //     get
+        //     {
+        //         return weight;
+        //     }
+        // }
+
+        public string Name {set; get;}
+        public float Height {set; get;}
+        public int Weight {set; get;}
 
         public List<string> pokemonInfo = new List<string>();
 
-        public void PokemonMethod()
+        public void GetPokemon()
         {
             // Fyller på listan med alla värden
-            name = UppercaseFirst(name);
-            height = height / 10;
-            pokemonInfo.Add("Name: " + name);
-            pokemonInfo.Add("Height: " + height + "m");
-            pokemonInfo.Add("Weight: " + weight + "kg");
-            
+            Name = UppercaseFirst(Name);
+            Height = Height / 10;
+            pokemonInfo.Add("Name: " + Name);
+            pokemonInfo.Add("Height: " + Height + "m");
+            pokemonInfo.Add("Weight: " + Weight + "kg");
+
             // Skriver ut alla värden
             System.Console.WriteLine();
             for (int i = 0; i < pokemonInfo.Count; i++)
--- Program.cs
+++ Program.cs
@@ -10,30 +10,44 @@ namespace Api_ovning
         {
             RestClient client = new RestClient("https://pokeapi.co/api/v2/");
 
-            System.Console.WriteLine("Which pokeon do you want to see information about?");
-            System.Console.WriteLine("Please enter a valid pokemon's name or ID");
-
-            // Hämtar informationen från api:et
-            string userPokemon = Console.ReadLine().Trim().ToLower();
-            RestRequest request = new RestRequest("pokemon/" + userPokemon);
-            IRestResponse response = client.Get(request);
-
-            // Kollar om Pokemonen existerar eller ej
-            // --> samt att man inte bara klickar på enter då api:et tillåter det
-            // Om Pokemonen inte existerar eller att användaren har klickat på enter så kommer hen bli tvungen att skriva in igenom
-            while (userPokemon == "" || (int)response.StatusCode != 200)
+            while (true)
             {
-                System.Console.WriteLine("\nPlease enter a valid pokemon's name or ID");
-                userPokemon = Console.ReadLine().Trim().ToLower();
-                request = new RestRequest("pokemon/" + userPokemon);
-                response = client.Get(request);
+                System.Console.WriteLine("\n\nWhich pokeon do you want to see information about?");
+                System.Console.WriteLine("Please enter a valid pokemon's name or ID");
+
+                // Hämtar informationen från api:et
+                string userPokemon = Console.ReadLine().Trim().ToLower();
+                RestRequest request = new RestRequest("pokemon/" + userPokemon);
+                IRestResponse response = client.Get(request);
+
+                // Kollar om Pokemonen existerar eller ej
+                // --> samt att man inte bara klickar på enter då api:et tillåter det
+                // Om Pokemonen inte existerar eller att användaren har klickat på enter så kommer hen bli tvungen att skriva in igenom
+                while (userPokemon == "" || (int)response.StatusCode != 200)
+                {
+                    System.Console.WriteLine("\nPlease enter a valid pokemon's name or ID");
+                    userPokemon = Console.ReadLine().Trim().ToLower();
+                    request = new RestRequest("pokemon/" + userPokemon);
+                    response = client.Get(request);
+                }
+
+                Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+
+                pokemon.GetPokemon();
+
+                System.Console.WriteLine("\n\nPress ESCAPE to quit\nOr any other key to continue");
+                ConsoleKeyInfo keyboardKey = Console.ReadKey();
+
+                if (keyboardKey.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.Clear();
+                }
             }
-
-            Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content);
-
-            pokemon.PokemonMethod();
-
-            Console.ReadLine();
         }
     }
 }
